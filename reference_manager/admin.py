a081668960a4facc03f72b47cd9f2d54ff5c4211
Betty Blaize <bethlakshmi@gmail.com>
2021-05-28 12:26:18 -0400

    admin side done, model tuned

--- reference_manager/admin.py
+++ reference_manager/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from reference_manager.models import *
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk',
+    	            'order',
+                    'name',
+                    'slug',
+                    'question',
+                    'instructions')
+    list_editable = ('order',
+                    'name',
+                    'slug',
+                    'question',
+                    'instructions')
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('pk',
+    	            'name',
+                    'slug',
+                    'category',
+                    'description',
+                    'source_count')
+    list_editable = ('name',
+                    'slug',
+                    'category',
+                    'description')
+    list_filter = ['category']
+
+    def source_count(self, obj):
+        return obj.urlsource_set.count() + obj.filesource_set.count()
+
+
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ('pk',
+    	            'name',
+                    'description',
+                    'created_at',
+                    'updated_at',
+                    'submitted_by')
+    list_filter = ['tags']
+
 
 # Register your models here.
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Profile)
+admin.site.register(FileSource, SourceAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(URLSource, SourceAdmin)
--- reference_manager/models.py
+++ reference_manager/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.db.models import (
     CASCADE,
     CharField,
+    DateTimeField,
+    ForeignKey,
     IntegerField,
     ManyToManyField,
     Model,
@@ -26,41 +28,64 @@ class Profile(Model):
     barony = CharField(max_length=250, blank=True)
     phone = CharField(max_length=50, blank=True)
     bio = TextField()
+    def __str__(self):
+        return self.display_name
     class Meta:
         app_label = "reference_manager"
+        ordering = ['display_name']
 
 
 class Category(Model):
-    name = CharField(max_length=128)
-    description = TextField(blank=True)
+    name = CharField(max_length=128, unique=True)
+    instructions = TextField(blank=True)
+    slug = SlugField(unique=True)
+    question = CharField(max_length=128)
+    order = IntegerField(unique=True)
+
+    def __str__(self):
+        return self.name
+
     class Meta:
         app_label = "reference_manager"
+        verbose_name_plural = 'categories'
+        ordering = ['order', 'name']
 
 
 class Tag(Model):
-    name = CharField(max_length=128)
+    name = CharField(max_length=128, unique=True)
     description = TextField(blank=True)
-    slug = SlugField()
-    category = OneToOneField(Category, on_delete=SET_NULL, null=True)
+    slug = SlugField(unique=True)
+    category = ForeignKey(Category, on_delete=SET_NULL, null=True)
+    def __str__(self):
+        return self.name
     class Meta:
         app_label = "reference_manager"
+        ordering = ['name']
+        unique_together = [('category', 'name'), ('category', 'slug')]
+
 
 
 class Source(Model):
     name = CharField(max_length=128)
     description = TextField(blank=True)
-    start_year = IntegerField(null=True)
-    end_year = IntegerField(null=True)
-    centuries = CharField(max_length=200, blank=True)
     tags = ManyToManyField(Tag)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+    submitted_by = ForeignKey(Profile,
+                              on_delete=SET_NULL,
+                              blank=True,
+                              null=True)
+    def __str__(self):
+        return self.name
 
     class Meta:
         app_label = "reference_manager"
         abstract = True
+        ordering = ['name']
 
 
 class URLSource(Source):
-    location = URLField()
+    location = URLField(unique=True)
     class Meta:
         app_label = "reference_manager"
 
