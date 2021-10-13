4d0877d0a9017c4a583c144f66ef806f9e5b4369
Betty Blaize <bethlakshmi@gmail.com>
2021-10-13 15:04:10 -0400

    Filter of dependancies and descriptions per position and source.

--- shastra_compedium/forms/position_detail_edit_form.py
+++ shastra_compedium/forms/position_detail_edit_form.py
@@ -57,6 +57,20 @@ class PositionDetailEditForm(ModelForm):
         queryset=PositionDetail.objects.filter(usage="Posture Description"),
         )
 
+    def __init__(self, *args, **kwargs):
+        super(PositionDetailEditForm, self).__init__(*args, **kwargs)
+        if 'instance' in kwargs:
+            detail = kwargs.get('instance')
+            self.fields['description'].queryset = PositionDetail.objects.filter(
+                usage="Posture Description",
+                position=detail.position,
+                sources__in=detail.sources.all(),
+                )
+            self.fields['dependencies'].queryset = PositionDetail.objects.filter(
+                usage="Posture Description",
+                sources__in=detail.sources.all(),
+                ).exclude(position=detail.position)
+
     class Meta:
         model = PositionDetail
         fields = [
