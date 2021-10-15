35b275e441ddcd6939cc498677c663883c2f08c6
Betty Blaize <bethlakshmi@gmail.com>
2021-10-15 16:11:43 -0400

    Basic for performer and dance style

--- shastra_compedium/forms/__init__.py
+++ shastra_compedium/forms/__init__.py
@@ -1,5 +1,6 @@
 from .chapter_form import (ChapterFormBasics, ChapterForm)
 from .chapter_detail_mapping import ChapterDetailMapping
+from .performer_form import PerformerForm
 from .position_detail_form import PositionDetailForm
 from .position_detail_edit_form import PositionDetailEditForm
 from .position_form import PositionForm
---
+++ shastra_compedium/views/make_performer.py
@@ -0,0 +1,43 @@
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+)
+from django_addanother.views import CreatePopupMixin, UpdatePopupMixin
+from shastra_compedium.models import Performer
+from django.urls import reverse_lazy
+from shastra_compedium.site_text import make_performer_messages
+from shastra_compedium.views import ShastraFormMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from shastra_compedium.forms import PerformerForm
+
+
+class PerformerCreate(LoginRequiredMixin,
+                      CreatePopupMixin,
+                      ShastraFormMixin,
+                      CreateView):
+    model = Performer
+    template_name = 'shastra_compedium/modal_make_form.tmpl'
+    success_url = reverse_lazy('position_list',
+                               urlconf="shastra_compedium.urls")
+    page_title = 'Performer'
+    view_title = 'Create Performer'
+    valid_message = make_performer_messages['create_success']
+    intro_message = make_performer_messages['create_intro']
+    form_class = PerformerForm
+
+
+class PerformerUpdate(LoginRequiredMixin,
+                      UpdatePopupMixin,
+                      ShastraFormMixin,
+                      UpdateView):
+    model = Performer
+    template_name = 'shastra_compedium/modal_make_form.tmpl'
+    success_url = reverse_lazy('position_list',
+                               urlconf="shastra_compedium.urls")
+    page_title = 'Performer'
+    view_title = 'Update Performer'
+    valid_message = make_performer_messages['edit_success']
+    intro_message = make_performer_messages['edit_intro']
+    form_class = PerformerForm
+    def get_queryset(self):
+        return self.model.objects.filter(contact=self.request.user)
