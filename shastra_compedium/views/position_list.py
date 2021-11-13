23fa5760d25cc8a6cbd7e70b52102e9c21989034
Betty Blaize <bethlakshmi@gmail.com>
2021-11-12 19:08:39 -0500

    Consolidate list buttons

--- shastra_compedium/views/position_list.py
+++ shastra_compedium/views/position_list.py
@@ -4,6 +4,7 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from shastra_compedium.models import (
+    ExampleImage,
     PositionDetail,
     Shastra,
     Source,
@@ -23,22 +24,31 @@ class PositionList(GenericList):
         if self.changed_obj == "Category":
             context['category_ids'] = context['changed_ids']
             context['changed_ids'] = []
+        elif self.changed_obj != "Position":
+            context['changed_ids'] = []
         return context
 
     def get_list(self):
         details = {}
         for detail in PositionDetail.objects.filter(description__isnull=True):
             if detail.position not in details:
-                details[detail.position] = {}
+                details[detail.position] = {"images": [],
+                                            "sources": {}}
             for source in detail.sources.all():
                 usage = detail.usage.replace(" ", "")
-                if source not in details[detail.position]:
-                    details[detail.position][source] = {usage: [detail],
-                                                        'num_details': 0}
-                elif usage not in details[detail.position][source]:
-                    details[detail.position][source][usage] = [detail]
+                if source not in details[detail.position]["sources"]:
+                    details[detail.position]["sources"][source] = {
+                        usage: [detail],
+                        'num_details': 0}
+                elif usage not in details[detail.position]["sources"][source]:
+                    details[detail.position]["sources"][source][usage] = [
+                        detail]
                 else:
-                    details[detail.position][source][usage] += [detail]
-                details[detail.position][source]['num_details'] = details[
-                    detail.position][source]['num_details'] + 1
+                    details[detail.position]["sources"][source][usage] += [
+                        detail]
+                details[detail.position]["sources"][source][
+                    'num_details'] = details[
+                        detail.position]["sources"][source]['num_details'] + 1
+        for image in ExampleImage.objects.all():
+            details[image.position]["images"] += [image]
         return details
