b43326a1ffb91a1c828c44ebf8b59b1f48a0a642
Betty Blaize <bethlakshmi@gmail.com>
2021-11-18 23:07:24 -0500

    remove create image, bulk upload is enough.

--- shastra_compedium/views/make_example_image.py
+++ shastra_compedium/views/make_example_image.py
@@ -11,21 +11,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from shastra_compedium.forms import ImageForm
 
 
-class ExampleImageCreate(LoginRequiredMixin,
-                         CreatePopupMixin,
-                         ShastraFormMixin,
-                         CreateView):
-    model = ExampleImage
-    template_name = 'shastra_compedium/modal_make_form.tmpl'
-    success_url = reverse_lazy('position_list',
-                               urlconf="shastra_compedium.urls")
-    page_title = 'Example Image'
-    view_title = 'Create Example Image'
-    valid_message = make_example_image_messages['create_success']
-    intro_message = make_example_image_messages['create_intro']
-    fields = ['position', 'general', 'details', 'dance_style', 'performer']
-
-
 class ExampleImageUpdate(LoginRequiredMixin,
                          UpdatePopupMixin,
                          ShastraFormMixin,
