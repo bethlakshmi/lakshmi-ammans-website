8ab55226e7546416b8a7d8cf25c164886b685050
Betty Blaize <bethlakshmi@gmail.com>
2022-07-19 22:27:24 -0400

    tune up list of combos

--- shastra_compedium/views/combination_list.py
+++ shastra_compedium/views/combination_list.py
@@ -13,4 +13,24 @@ class CombinationList(GenericList):
         return context
 
     def get_list(self):
-        return {"combinations": CombinationDetail.objects.all()}
+        subject_dict  = {}
+        for detail in CombinationDetail.objects.all():
+            key = "NONE"
+
+            if detail.subject is not None:
+                key = detail.subject
+            if key in subject_dict:
+                subject_dict[key]['combos'] += [detail]
+            else:
+                subject_dict[key] = {'combos': [detail],
+                                     'images': [],
+                                     'sources': []}
+
+            for image in detail.exampleimage_set.all():
+                if image not in subject_dict[key]['images']:
+                    subject_dict[key]['images'] += [image]
+            for source in detail.sources.all():
+                if source not in subject_dict[key]['sources']:
+                    subject_dict[key]['sources'] += [source]
+
+        return {"combinations": subject_dict}
